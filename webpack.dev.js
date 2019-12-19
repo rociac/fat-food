70fe36f3d5077f425a52bfa7b9a3f19b0d212edf
Rodolfo <brsrkdev@gmail.com>
2019-12-19 16:45:53 -0600

    Fix linter errors

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,16 +1,16 @@
 const path = require('path');
-const common = require('./webpack.common');
 const merge = require('webpack-merge');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const common = require('./webpack.common');
 
 module.exports = merge(common, {
   mode: 'development',
   output: {
     filename: '[name].bundle.js',
-    path: path.resolve(__dirname, "dist")
+    path: path.resolve(__dirname, 'dist'),
   },
   plugins: [new HtmlWebpackPlugin({
-    template: './src/template.html'
+    template: './src/template.html',
   })],
   module: {
     rules: [
@@ -19,9 +19,9 @@ module.exports = merge(common, {
         use: [
           'style-loader',
           'css-loader',
-          'sass-loader'
-        ]
-      }
-    ]
-  }
+          'sass-loader',
+        ],
+      },
+    ],
+  },
 });
